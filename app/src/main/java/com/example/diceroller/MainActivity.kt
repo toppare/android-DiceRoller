ab7f80a804a98dd9da3bb3f3df5eb6f92db56c99
Baran Toppare <barantoppare@gmail.com>
2020-01-26 18:14:55 +0100

    rolling changes number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+
+
         rollButton.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-            
+            rollDice()
         }
+
+
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..7).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
 
     }
 }
